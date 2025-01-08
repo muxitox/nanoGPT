a2768124a9e1915ce053a61d37f93d6a2cefaa54
muxitox <angel.poc.lopez@gmail.com>
2025-01-08 11:43:31 +0100

    Change config

--- config/train_shakespeare_mingpt.py
+++ config/train_shakespeare_mingpt.py
@@ -2,7 +2,7 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-shakespeare'
-eval_interval = 250 # keep frequent because we'll overfit
+eval_interval = 500 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 50 # don't print too too often
 
@@ -14,9 +14,9 @@ wandb_project = 'shakespeare'
 wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare'
-gradient_accumulation_steps = 8
+gradient_accumulation_steps = 1
 batch_size = 12
-block_size = 64 # context of up to 256 previous characters
+block_size = 64 # context of up to 64 previous characters
 
 # baby GPT model :)
 n_layer = 2
@@ -25,21 +25,17 @@ n_embd = 128
 dropout = 0.0 # No dropout
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 20000
-lr_decay_iters = 20000 # make equal to max_iters usually
+max_iters = 10000
+lr_decay_iters = 10000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
-# on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+# on macbook and desktop with no cpu also add
+device = 'cpu'  # run on cpu only
+compile = False # do not torch compile the model
 
 # on Hypatia
 # device = 'cuda'  # run on cpu only
 # compile = False # do not torch compile the model
-
-# on desktop
-device = 'cpu'  # run on cpu only
-compile = True # do not torch compile the model
