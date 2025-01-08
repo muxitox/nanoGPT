13f87e56decb4c12bbe5193c77cebebf5b504ca0
muxitox <angel.poc.lopez@gmail.com>
2025-01-08 12:49:40 +0100

    Update config

--- config/train_shakespeare_mingpt_gpu.py
+++ config/train_shakespeare_mingpt_gpu.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-shakespeare'
+out_dir = 'out-shakespeare-gpu'
 eval_interval = 500 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 50 # don't print too too often
