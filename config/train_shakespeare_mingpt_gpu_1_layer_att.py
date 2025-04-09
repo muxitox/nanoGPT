20db564e23d859ec4655a605db9082b5c50d7ff2
Ángel Poc <muxitox>
2025-04-09 13:27:52 +0100

    Change config

--- config/train_shakespeare_mingpt_gpu_1_layer_att.py
+++ config/train_shakespeare_mingpt_gpu_1_layer_att.py
@@ -2,7 +2,7 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-shakespeare-gpu-1-layer-att'
-eval_interval = 500 # keep frequent because we'll overfit
+eval_interval = 100 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 50 # don't print too too often
 
@@ -25,8 +25,8 @@ n_embd = 768
 dropout = 0.01 # No dropout
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 1000
+lr_decay_iters = 1000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
