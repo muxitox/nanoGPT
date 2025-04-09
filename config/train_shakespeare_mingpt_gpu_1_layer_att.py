606b6872a665bbf634537ed301dde1ca296c9d69
Ángel Poc <muxitox>
2025-04-09 12:07:32 +0100

    Add notebooks for 1 att layer

--- config/train_shakespeare_mingpt_gpu_1_layer_att.py
+++ config/train_shakespeare_mingpt_gpu_1_layer_att.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-shakespeare-gpu'
+out_dir = 'out-shakespeare-gpu-1-layer-att'
 eval_interval = 500 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 50 # don't print too too often
@@ -14,19 +14,19 @@ wandb_project = 'shakespeare_1_layer_att'
 wandb_run_name = 'mini-gpt-1-layer-att'
 
 dataset = 'shakespeare'
-gradient_accumulation_steps = 5
-batch_size = 12
-block_size = 256 # context of up to 64 previous characters
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 768 # context of up to 768 previous characters
 
 # baby GPT model :)
 n_layer = 1
-n_head = 6
-n_embd = 384
-dropout = 0.0 # No dropout
+n_head = 12
+n_embd = 1024
+dropout = 0.01 # No dropout
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 10000
-lr_decay_iters = 10000 # make equal to max_iters usually
+max_iters = 5000
+lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
