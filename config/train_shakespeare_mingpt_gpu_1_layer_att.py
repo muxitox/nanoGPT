07401221f3bb6396f91a5854c162856204455881
Ángel Poc <muxitox>
2025-04-09 15:45:10 +0100

    Update config

--- config/train_shakespeare_mingpt_gpu_1_layer_att.py
+++ config/train_shakespeare_mingpt_gpu_1_layer_att.py
@@ -16,7 +16,7 @@ wandb_run_name = 'mini-gpt-1-layer-att'
 dataset = 'shakespeare'
 gradient_accumulation_steps = 4
 batch_size = 16
-block_size = 768 # context of up to 768 previous characters
+block_size = 512 # context of up to 768 previous characters
 
 # baby GPT model :)
 n_layer = 1
@@ -25,10 +25,10 @@ n_embd = 768
 dropout = 0.01 # No dropout
 
 learning_rate = 1e-4 # with baby networks can afford to go a bit higher
-max_iters = 1500
-lr_decay_iters = 1500 # make equal to max_iters usually
+max_iters = 1000
+lr_decay_iters = 1000 # make equal to max_iters usually
 min_lr = 1e-5 # learning_rate / 10 usually
-beta2 = 0.95 # make a bit bigger because number of tokens per iter is small
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
